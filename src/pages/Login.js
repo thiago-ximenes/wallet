fc45d3b85a2b183382d821e80c3d95768b9f9666
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-01-10 19:21:48 -0300

    working on email validation.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,36 @@
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from 'react';
-import Box from '@material-ui/core/Box';
+import validator from 'validator';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: false,
+    };
+  }
+
+  validatorEmail = (e) => {
+    const { value } = e.target;
+    if (validator.isEmail(value)) {
+      this.setState({ isDisabled: true });
+
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { email, password, isDisabled } = this.state;
+    const { handleChange } = this;
     return (
       <Box mt={ 5 }>
         <Grid
@@ -17,6 +42,12 @@ class Login extends React.Component {
           <Box mb={ 0.9 }>
             <Grid item xs>
               <TextField
+                name="email"
+                onChange={ (e) => {
+                  handleChange(e);
+                  validatorEmail(e);
+                } }
+                value={ email }
                 data-testid="email-input"
                 variant="outlined"
                 label="Email"
@@ -25,6 +56,9 @@ class Login extends React.Component {
           </Box>
           <Grid item xs>
             <TextField
+              name="password"
+              type="password"
+              onChange={ handleChange }
               data-testid="password-input"
               variant="outlined"
               label="Password"
@@ -33,7 +67,11 @@ class Login extends React.Component {
           <Grid item xs>
             <Box mt={ 0.5 }>
               <Button
+                disabled={ isDisabled }
+                color="primary"
                 variant="contained"
+                size="large"
+                endIcon={ <ExitToAppIcon /> }
               >
                 Entrar
               </Button>
