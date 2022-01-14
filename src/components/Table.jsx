eaaacab8b2c43ee530d71ca6cdeb0d396190093b
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-01-14 10:56:33 -0300

    working at Table component.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,10 +3,41 @@ import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>
-        vai
-      </div>
+      <table>
+        <tr>
+          <th>
+            Descrição
+          </th>
+          <th>
+            Tag
+          </th>
+          <th>
+            Método de pagamento
+          </th>
+          <th>
+            Valor
+          </th>
+          <th>
+            Moeda
+          </th>
+          <th>
+            Câmbio utilizado
+          </th>
+          <th>
+            Valor convertido
+          </th>
+          <th>
+            Moeda de conversão
+          </th>
+        </tr>
+        { expenses.lenght > 0 && expenses.map((expense) => (
+          <tr key={ expense.id }>
+            
+          </tr>
+        )) }
+      </table>
     );
   }
 }
