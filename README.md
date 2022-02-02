47f22a62b363805873f37fb37eae9627ba81bc3c
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-02-02 08:55:24 -0300

    edit table code to have less then 250 lines and update README.

--- README.md
+++ README.md
@@ -31,6 +31,30 @@ yarn start
 ```
 
 ## How to use
-1. Login with an email and type anything more then 6 characters
-2. Add a value
-3. Add a description of this entrance
+1. Login with an email and type anything more then 6 characters.
+
+![Login](/login.gif)
+
+2. EData Entrance:
+    - Add a value of your expense
+    - Add a description of this entrance
+    - Select the currency
+    - Choose the tag of entrance
+    - Add a payment method
+    - Click on "Adicionar Despesas" to input the data
+
+![Data Entrance](/data-entrance.gif)
+
+3. To delete one entrance, just click on "trash" icon.
+
+![Delete Data](/delete.gif)
+
+4. To edit one entrance, simple click on "backspace" icon to open a input at the same line and input the refactored data:
+    - Add the new value
+    - Add the new description
+    - Select the new currency
+    - Choose the new tag of entrance
+    - Add the new payment method
+    - Click on o "save" icon to save the new status
+
+![Edit Data](/edit.gif)
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,6 +5,7 @@ import { updateExpense } from '../actions';
 import { paymentMethods, tags } from '../data/optionsToSelect';
 import Input from './Input';
 import Select from './Select';
+import edit from '../helpers/editIcon';
 
 class Table extends Component {
   constructor() {
@@ -152,15 +153,7 @@ class Table extends Component {
                         active:scale-110"
                       onClick={ () => this.editExpenseFromStore(expense, index) }
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2
-                            0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
-                        />
-                      </svg>
+                      { edit }
                     </button>
                   </div>
                 </td>
